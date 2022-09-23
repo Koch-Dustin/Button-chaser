a58756ee17ce23c3818e9f1aae73e25da3dfc2c7
Dustin Koch <dustin.koch@5minds.de>
2022-09-23 07:50:56 +0200

    Worked on code

---
+++ Version3/index.js
@@ -0,0 +1,50 @@
+const button = document.getElementById('button')
+const OFFSET = 100
+
+button.addEventListener('click', () => {
+  alert('You won')
+  window.close()
+})
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.pageX
+  const y = e.pageY
+  const buttonBox = button.getBoundingClientRect()
+  const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+  const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+  
+  
+
+})
+
+function setButtonPosition(left, top) {
+  const windowBox = document.body.getBoundingClientRect()
+  const buttonBox = button.getBoundingClientRect()
+
+  if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+    left = window.innerWidth / 2
+    top = window.innerHeight / 2
+  }
+
+  if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+    left = window.innerWidth / 2
+    top = window.innerHeight / 2
+  }
+
+  if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+    left = window.innerWidth / 2
+    top = window.innerHeight / 2
+  }
+  
+  if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+    left = window.innerWidth / 2
+    top = window.innerHeight / 2
+  }
+
+  button.style.left = `${left}px`
+  button.style.top = `${top}px`
+}
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
+  return boxPosition - mousePosition + boxSize / 2
+}
